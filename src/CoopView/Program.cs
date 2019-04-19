107f2ea8eb6deea9a6df179488e6453b85a46466
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-04-19 10:51:32 +0200

    Change project to CLI

--- src/CoopView/Program.cs
+++ src/CoopView/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
-using CoopView.Forms;
-
-namespace CoopView
+﻿namespace CoopView
 {
     /// <summary>
     /// Entry point of the program.
@@ -13,12 +9,8 @@ namespace CoopView
         /// The main entry point for the application.
         /// </summary>
         /// <param name="args">Program arguments.</param>
-        [STAThread]
         public static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
         }
     }
 }
