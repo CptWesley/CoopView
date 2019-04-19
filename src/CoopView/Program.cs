f34e629865d2c846b941348ae8e1cb6c9eb61719
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-04-19 15:21:19 +0200

    Add cli mode selection switch statement

--- src/CoopView/Program.cs
+++ src/CoopView/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CoopView
+﻿using System;
+
+namespace CoopView
 {
     /// <summary>
     /// Entry point of the program.
@@ -11,6 +13,45 @@
         /// <param name="args">Program arguments.</param>
         public static void Main(string[] args)
         {
+            switch (GetArgument(args, 0))
+            {
+                case "host":
+                case "h":
+                    break;
+                case "client":
+                case "c":
+                    break;
+                default:
+                    ErrorExit("Usage:" + Environment.NewLine +
+                        "Client mode: client <ip> <port> <window name> <width> <height> <fps>" + Environment.NewLine +
+                        "Host mode: host <port>");
+                    break;
+            }
         }
+
+        /// <summary>
+        /// Exits the program with the given error message.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        private static void ErrorExit(string message)
+        {
+            Console.Error.WriteLine(message);
+            int hash = message.GetHashCode();
+            if (hash == 0)
+            {
+                hash++;
+            }
+
+            Environment.Exit(hash);
+        }
+
+        /// <summary>
+        /// Gets the argument at the index.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <param name="index">The index.</param>
+        /// <returns>The argument at the index, null otherwise.</returns>
+        private static string GetArgument(string[] args, int index)
+            => index < args.Length ? args[index] : null;
     }
 }
