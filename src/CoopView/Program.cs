5803f58d0922897e930c70bea1122722b3edd6ac
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-04-19 10:45:28 +0200

    Add basic program

---
+++ src/CoopView/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using CoopView.Forms;
+
+namespace CoopView
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">Program arguments.</param>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
